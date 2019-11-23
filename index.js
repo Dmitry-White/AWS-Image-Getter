fd7072dc54c9d31c0b3e8d3301720006b036f9be
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-23 17:25:06 +0300

    Add nodemon

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/:name', (req, res) => res.send(`${req.params.name} Image!`));
+app.post('/', (req, res) => res.send('Post Image!'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
