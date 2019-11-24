2f4edb4e37beed1225f3210d1f0cc619a4955cca
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-24 22:41:43 +0300

    Add initial image upload

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -1,3 +1,9 @@
-const postHandler = (req, res) => res.send('Post Image!');
+const postHandler = (req, res) => {
+  const image = req.files;
+  const imageBody = req.body;
+  console.log(image);
+  console.log(imageBody);
+  res.send('Image Uploaded');
+}
 
 export default postHandler;
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import multer from 'multer';
 
 import { getHandler, postHandler } from './handlers/index.js';
 
 const PORT = 3000;
 const app = express();
+const upload = multer({ dest: 'assets/' });
 
 app.use(morgan('dev'));
 
 app.get('/:name', getHandler);
-app.post('/', postHandler);
+app.post('/', upload.any(), postHandler);
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
