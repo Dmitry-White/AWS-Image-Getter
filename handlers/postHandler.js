f5d7230dd7f62174bd56b8dd4a942bf8a61db230
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-25 14:41:30 +0300

    Add multer upload callback

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -1,9 +1,16 @@
 import { uploadImage } from '../services/index.js'
 
 const postHandler = (req, res) => {
-  uploadImage(req, res);
-  
-  res.send('Image Uploaded');
+  uploadImage(req, res, (err) => {
+    if(err) {
+      res.render('index', {
+        uploadedMessage: err
+      });
+    } else {
+      console.log(req.file);
+      res.send('Image Uploaded');
+    }
+  });
 };
 
 export default postHandler;
