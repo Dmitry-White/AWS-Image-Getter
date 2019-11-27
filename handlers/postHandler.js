115da89179ba37500e125f9b4f5f043ed0ba1e47
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-27 15:31:33 +0300

    Add error handling

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -1,19 +1,27 @@
 import { uploadImage } from '../services/index.js';
 
 const postHandler = (req, res) => {
+  const templateVariables = {
+    uploadedMessage: '',
+  };
+
   uploadImage(req, res, (err) => {
     if (err) {
-      const templateVariables = {
-        uploadedMessage: err,
-      };
+      templateVariables.uploadedMessage = err;
 
       console.log(templateVariables);
       res.render('index', templateVariables);
     } else {
       const dataForRDS = req.file;
 
+      if (dataForRDS === undefined) {
+        templateVariables.uploadedMessage = 'Error: No Image Selected!';
+      } else {
+        templateVariables.uploadedMessage = `${dataForRDS.originalname} Uploaded`;
+      }
+
       console.log(dataForRDS);
-      res.send(`${dataForRDS.originalname} Uploaded`);
+      res.render('index', templateVariables);
     }
   });
 };
